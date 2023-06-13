94c581add5cde69f1cbdbfbb44dba6bd682e28eb
Michalinaaaa <136478286+Michalinaaaa@users.noreply.github.com>
2023-06-13 18:12:09 +0200

    Add files via upload

---
+++ struktury_2c.cpp
@@ -0,0 +1,243 @@
+﻿#include <iostream>
+#include <vector>
+#include <limits>
+#include <chrono>
+#include <unordered_set>
+#include <cstdlib>
+
+using namespace std;
+
+struct Krawedz {
+    int cel;
+    int waga;
+};
+
+class Stos {
+private:
+    int* tab;
+    int top_element;
+    int rozmiar;
+    int push_ilosc;
+    int pop_ilosc;
+    int kroki_push;
+    int kroki_pop;
+
+public:
+    Stos() {
+        tab = nullptr;
+        top_element = -1;
+        rozmiar = 0;
+        push_ilosc = 0;
+        pop_ilosc = 0;
+        kroki_push = 0;
+        kroki_pop = 0;
+    }
+
+    ~Stos() {
+        delete[] tab;
+    }
+
+    void push(int x) {
+        if (czy_pelny()) {
+            int* new_tab = new int[2 * rozmiar + 1];
+            for (int i = 0; i <= top_element; i++) {
+                new_tab[i] = tab[i];
+                push_ilosc++;
+            }
+            rozmiar = 2 * rozmiar + 1;
+            delete[] tab;
+            tab = new_tab;
+            push_ilosc++;
+        }
+
+        top_element++;
+        tab[top_element] = x;
+        push_ilosc++;
+        kroki_push++;
+    }
+
+    int pop() {
+        if (czy_pusty()) {
+            cout << "Stos jest pusty. Nie można usunąć elementu." << endl;
+            return -1;
+        }
+
+        int value = tab[top_element];
+        top_element--;
+
+        if (top_element < rozmiar / 2) {
+            int* new_tab = new int[rozmiar / 2 + 1];
+            for (int i = 0; i <= top_element; i++) {
+                new_tab[i] = tab[i];
+                pop_ilosc++;
+            }
+            new_tab[top_element + 1] = 0;
+            rozmiar = rozmiar / 2 + 1;
+            delete[] tab;
+            tab = new_tab;
+            pop_ilosc++;
+        }
+
+        pop_ilosc++;
+        kroki_pop++;
+        return value;
+    }
+
+    int wyswietl() {
+        if (czy_pusty()) {
+            cout << "BLAD! Stos jest pusty." << endl;
+            return -1;
+        }
+
+        return tab[top_element];
+    }
+
+    bool czy_pusty() {
+        return top_element == -1;
+    }
+
+    bool czy_pelny() {
+        return top_element == rozmiar - 1;
+    }
+
+    int push_wyswietl() {
+        return push_ilosc;
+    }
+
+    int pop_wyswietl() {
+        return pop_ilosc;
+    }
+
+    int push_kroki() {
+        return kroki_push;
+    }
+
+    int pop_kroki() {
+        return kroki_pop;
+    }
+
+    void wyswietl_graf(const vector<vector<Krawedz>>& graf) {
+        int liczba_wierzcholkow = graf.size();
+
+        for (int i = 0; i < liczba_wierzcholkow; ++i) {
+            cout << "Wierzcholek " << i << ": ";
+            for (const Krawedz& krawedz : graf[i]) {
+                cout << "(" << krawedz.cel << ", " << krawedz.waga << ") ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+const int INF = numeric_limits<int>::max();
+
+using Graf = vector<vector<Krawedz>>;
+
+void dodajKrawedz(Graf& graf, int zrodlo, int cel, int waga) {
+    // czy krawędź już istnieje
+    for (const Krawedz& krawedz : graf[zrodlo]) {
+        if (krawedz.cel == cel) {
+            return;  // Krawędź już istnieje
+        }
+    }
+
+    Krawedz krawedz;
+    krawedz.cel = cel;
+    krawedz.waga = waga;
+
+    graf[zrodlo].push_back(krawedz);
+
+    // Dodanie  krawędzi w drugą stronę
+    Krawedz krawedz_odwrotna;
+    krawedz_odwrotna.cel = zrodlo;
+    krawedz_odwrotna.waga = waga;
+
+    graf[cel].push_back(krawedz_odwrotna);
+}
+
+vector<int>  Dijkstra(const Graf& graf, int zrodlo, Stos& stos) {
+    int liczba_wierzcholkow = graf.size();
+    vector<int> odleglosci(liczba_wierzcholkow, INF);
+    odleglosci[zrodlo] = 0;
+
+    stos.push(zrodlo);
+
+    while (!stos.czy_pusty()) {
+        int obecny = stos.pop();
+
+        if (odleglosci[obecny] == INF) {
+            continue;
+        }
+
+        for (const Krawedz& krawedz : graf[obecny]) {
+            int cel = krawedz.cel;
+            int waga = krawedz.waga;
+
+            if (odleglosci[obecny] + waga < odleglosci[cel]) {
+                odleglosci[cel] = odleglosci[obecny] + waga;
+                stos.push(cel);
+            }
+        }
+    }
+
+    return odleglosci;
+}
+
+void wykonaj(const Graf& graf, int liczba_wierzcholkow, Stos& stos) {
+    // Wyświetlanie grafu
+    cout << "Graf:\n";
+    stos.wyswietl_graf(graf);
+
+    // Pomiar czasu wykonania
+    auto start = chrono::steady_clock::now();
+    int zrodlo = 0;
+    vector<int> odleglosci = Dijkstra(graf, zrodlo, stos);
+    auto end = chrono::steady_clock::now();
+
+    // Wyświetlanie wyników
+    cout << "Wyniki dla grafu z " << liczba_wierzcholkow << " wierzcholkami:\n";
+    cout << "Czas wykonania: " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << " ms\n";
+     cout << "Najkrotsze drogi:\n";
+     for (int i = 0; i < liczba_wierzcholkow; ++i) {
+         cout << "Od wierzcholka " << zrodlo << " do wierzcholka " << i << ": ";
+         if (odleglosci[i] == INF) {
+             cout << "brak sciezki" << std::endl;
+         }
+         else {
+             cout << odleglosci[i] << std::endl;
+         }
+     }
+
+    cout << "--------------------------------------\n";
+}
+
+int main() {
+    vector<int> liczba_wierzcholkow_lista = { 5, 10 };
+
+    for (int liczba_wierzcholkow : liczba_wierzcholkow_lista) {
+        Graf graf(liczba_wierzcholkow);
+
+        // Tworzenie losowego grafu 
+        for (int i = 0; i < liczba_wierzcholkow; ++i) {
+            int liczba_krawedzi = rand() % (liczba_wierzcholkow - 1) + 1;
+            unordered_set<int> polaczone_wierzcholki;
+
+            while (polaczone_wierzcholki.size() < liczba_krawedzi) {
+                int cel = rand() % liczba_wierzcholkow;
+                int waga = rand() % 10 + 1;
+
+                if (cel != i && polaczone_wierzcholki.count(cel) == 0) {
+                    dodajKrawedz(graf, i, cel, waga);
+                    polaczone_wierzcholki.insert(cel);
+                }
+            }
+        }
+
+        
+        Stos stos_1;
+
+        wykonaj(graf, liczba_wierzcholkow, stos_1);
+    }
+
+    return 0;
+}
---
+++ struktury_a.cpp
@@ -0,0 +1,170 @@
+﻿#include <iostream>
+#include <limits>
+#include <chrono>
+#include <random>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+using namespace std::chrono;
+
+const int NIESKONCZONOSC = numeric_limits<int>::max();
+
+struct Wezel {
+    int odleglosc;
+    int wierzcholek;
+    Wezel* nastepny;
+};
+
+class KolejkaPriorytetowa {
+private:
+    Wezel* glowa;
+
+public:
+    KolejkaPriorytetowa() : glowa(nullptr) {}
+
+    void dodaj(int odleglosc, int wierzcholek) {
+        Wezel* nowyWezel = new Wezel;
+        nowyWezel->odleglosc = odleglosc;
+        nowyWezel->wierzcholek = wierzcholek;
+        nowyWezel->nastepny = nullptr;
+
+        if (glowa == nullptr || odleglosc < glowa->odleglosc) {
+            nowyWezel->nastepny = glowa;
+            glowa = nowyWezel;
+        }
+        else {
+            Wezel* aktualny = glowa;
+            while (aktualny->nastepny != nullptr && aktualny->nastepny->odleglosc <= odleglosc) {
+                aktualny = aktualny->nastepny;
+            }
+            nowyWezel->nastepny = aktualny->nastepny;
+            aktualny->nastepny = nowyWezel;
+        }
+    }
+
+    bool pusta() {
+        return glowa == nullptr;
+    }
+
+    int topWierzcholek() {
+        return glowa->wierzcholek;
+    }
+
+    int topOdleglosc() {
+        return glowa->odleglosc;
+    }
+
+    void usun() {
+        if (glowa != nullptr) {
+            Wezel* temp = glowa;
+            glowa = glowa->nastepny;
+            delete temp;
+        }
+    }
+};
+
+void Dijkstra(int** graf, int n, int zrodlo) {
+    int* odleglosci = new int[n];
+    for (int i = 0; i < n; i++) {
+        odleglosci[i] = NIESKONCZONOSC;
+    }
+
+    odleglosci[zrodlo] = 0;
+
+    KolejkaPriorytetowa kolejka;
+    kolejka.dodaj(odleglosci[zrodlo], zrodlo);
+
+    while (!kolejka.pusta()) {
+        int u = kolejka.topWierzcholek();
+        kolejka.usun();
+
+        for (int v = 0; v < n; v++) {
+            if (graf[u][v] && odleglosci[u] + graf[u][v] < odleglosci[v]) {
+                odleglosci[v] = odleglosci[u] + graf[u][v];
+                kolejka.dodaj(odleglosci[v], v);
+            }
+        }
+    }
+
+    cout << "Dlugosci przebytych drog dla grafu o rozmiarze " << n << " i zrodle " << zrodlo << ":\n";
+    for (int i = 0; i < n; i++) {
+        cout << "Do wierzcholka " << i << ": " << odleglosci[i] << endl;
+    }
+
+    delete[] odleglosci;
+}
+
+void GenerujGraf(int** graf, int n) {
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dist(1, 10);
+
+    for (int i = 0; i < n; i++) {
+        int liczba_krawedzi = i;
+
+        vector<int> wierzcholki;
+        for (int j = 0; j < n; j++) {
+            if (j != i) {
+                wierzcholki.push_back(j);
+            }
+        }
+        shuffle(wierzcholki.begin(), wierzcholki.end(), gen);
+
+        for (int j = 0; j < liczba_krawedzi; j++) {
+            int v = wierzcholki[j];
+            int waga = dist(gen);
+            graf[i][v] = waga;
+            graf[v][i] = waga;
+        }
+    }
+}
+
+void WyswietlGraf(int** graf, int n) {
+    cout << "Graf:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << graf[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int rozmiary[] = { 5 };
+    int liczba_rozmiary = sizeof(rozmiary) / sizeof(rozmiary[0]);
+
+    for (int i = 0; i < liczba_rozmiary; i++) {
+        int n = rozmiary[i];
+
+        int** graf = new int* [n];
+        for (int j = 0; j < n; j++) {
+            graf[j] = new int[n];
+            for (int k = 0; k < n; k++) {
+                graf[j][k] = 0;
+            }
+        }
+
+        GenerujGraf(graf, n);
+
+        WyswietlGraf(graf, n);
+
+        auto start = high_resolution_clock::now();
+
+        Dijkstra(graf, n, 0);
+
+        auto end = high_resolution_clock::now();
+        auto czas_trwania = duration_cast<milliseconds>(end - start);
+
+        cout << "Czas wykonywania dla grafu o rozmiarze " << n << ": " << czas_trwania.count() << " ms\n";
+
+        for (int j = 0; j < n; j++) {
+            delete[] graf[j];
+        }
+        delete[] graf;
+
+        cout << "---------------------------------\n";
+    }
+
+    return 0;
+}
