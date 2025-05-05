c7b275f7f9a7b3fa8d91c467d8388c5037c4104a
Michalina <264278@student.pwr.edu.pl>
2025-05-05 23:49:35 +0200

    nazwa

--- struktury_2c.cpp
+++ struktury_2c.cpp
@@ -208,7 +208,7 @@ void wykonaj(const Graf& graf, int liczba_wierzcholkow, Stos& stos) {
          }
      }
 
-    cout << "--------------------------------------cos,cos,cos\n";
+    cout << "---------------------------------dsssfsfsf-----cos,cos,cos\n";
 }
 
 int main() {
